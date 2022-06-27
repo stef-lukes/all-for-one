8940d6f70bc71d5fdd1befe59d82cf6d1cbf93e4
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-06-27 13:00:51 +0100

    Added server file

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// app.use(express.json());
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
