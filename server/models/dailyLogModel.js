424f180635e7060c5ba4aa7821aaa00fd07bd396
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-06-28 12:19:40 +0100

    Created dailyLogs Model

---
+++ server/models/dailyLogModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const dailyLogSchema = mongoose.Schema(
+  {
+    activityName: {
+      type: String,
+      required: [true, "Add activity name"],
+    },
+    bodyText: {
+      type: String,
+      required: [true, "Add bodyText"],
+    },
+    categories: {
+      type: [String],
+      required: [true, ["", "", ""]],
+    },
+    colour: {
+      type: String,
+      required: false,
+    },
+    order: {
+      type: Number,
+      required: true,
+    },
+    isRecurring: {
+      type: Boolean,
+      required: [true, "Is this activity recurring?"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("dailyLogModel", dailyLogSchema);
