3734bbc1309e5d731a95a41c74099c023fc7d83e
Alex Davis <alexdevdavis@gmail.com>
2022-06-30 20:36:37 +0100

    refactor user context to register/login new user then redirect to dashboard with greeting

--- client/all-for-one-fe/src/App.tsx
+++ client/all-for-one-fe/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import { UserContext } from "./contexts/AuthProvider";
 
 function App() {
+  const [user, setUser] = useState()
+
   return (
-    <>
+    <UserContext.Provider value={{user, setUser}}>
       <Router>
         <div className="App">
           <Routes>
@@ -17,7 +21,7 @@ function App() {
           </Routes>
         </div>
       </Router>
-    </>
+    </UserContext.Provider>
   );
 }
 
