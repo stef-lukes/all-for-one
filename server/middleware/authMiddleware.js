fa3c6a05112715fc34f641f82883a08980d026e1
Alex Davis <alexdevdavis@gmail.com>
2022-07-05 11:59:09 +0100

    update authMiddleware to read from .env secret

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -10,12 +10,12 @@ const protect = asyncHandler(async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (error) {
       res.status(401);
-      throw new Error("Not authorized");
+      throw new Error(`Not authorized: ${error}`);
     }
   }
   if (!token) {
