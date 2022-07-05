8cd39772f5441b359bc8365b089377a249807289
Alex Davis <alexdevdavis@gmail.com>
2022-07-05 15:07:05 +0100

    create UserAdmin component for managing hub users

---
+++ client/all-for-one-fe/src/pages/UserAdmin.jsx
@@ -0,0 +1,9 @@
+import {React, useEffect, useContext} from 'react';
+import { UserContext } from '../contexts/AuthProvider';
+
+
+export default function UserAdmin() {
+  return (
+    <div>UserAdmin</div>
+  )
+}
