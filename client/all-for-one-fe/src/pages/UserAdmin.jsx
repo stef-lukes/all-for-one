2a131c57697fc96730822db62f1320e27cc66563
Alex Davis <alexdevdavis@gmail.com>
2022-07-06 08:59:56 +0100

    set out basic component requirements in client-side user admin

--- client/all-for-one-fe/src/pages/UserAdmin.jsx
+++ client/all-for-one-fe/src/pages/UserAdmin.jsx
@@ -1,9 +1,29 @@
-import {React, useEffect, useContext} from 'react';
+import {React, useEffect, useContext, useState} from 'react';
 import { UserContext } from '../contexts/AuthProvider';
+import { Navigate } from 'react-router-dom';
+import { HubContext } from '../contexts/HubProvider';
+import AssignHub from "../components/AssignHub"
 
 
 export default function UserAdmin() {
+    const {user, setUser} = useContext(UserContext) 
+    const [hub, setHub] = useContext(HubContext)
+
+    useEffect(() => {
+        const stringFromStorage = localStorage.getItem("all-for-one-user")
+        if (!user && stringFromStorage) {
+          const storedUser = JSON.parse(stringFromStorage);
+          console.log(storedUser, "<<<<< user from local storage")
+          setUser(storedUser)
+        }
+      }, [])
+
+
   return (
-    <div>UserAdmin</div>
+    <>
+    <AssignHub/>
+    <InviteUsers/>
+    <UserManagement/>
+    </>
   )
 }
