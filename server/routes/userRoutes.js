b3a69f3333eb573a96fb03e7b2853d5c55820e2a
Alex Davis <alexdevdavis@gmail.com>
2022-07-06 08:56:31 +0100

    move admin-user express controller functions to hub controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const {
   getUsers,
   setUser,
-  updateUser,
-  deleteUser,
   loginUser,
-  getMe,
-  inviteUser,
 } = require("../controllers/userControllers");
 
 const { protect } = require("../middleware/authMiddleware");
@@ -16,6 +12,5 @@ router.route("/").get(getUsers).post(setUser);
 router.route("/login").post(loginUser);
 router.route("/me").get(protect, getMe);
 router.route("/addusers").post(inviteUser);
-router.route("/:user_id").put(updateUser).delete(deleteUser);
 
 module.exports = router;
