59f3c095779a19074799987b393d16e2a8e5e37e
Alex Davis <alexdevdavis@gmail.com>
2022-07-06 14:54:54 +0100

    add /hub react route to render HubAdmin page

--- client/all-for-one-fe/src/App.tsx
+++ client/all-for-one-fe/src/App.tsx
@@ -9,6 +9,7 @@ import { HubContext } from "./contexts/HubProvider";
 import LifeStory from "./components/LifeStory";
 import FamilyTree from "./components/FamilyTree";
 import LifeStoryCategory from "./components/LifeStoryCategory";
+import HubAdmin from "./pages/HubAdmin";
 
 function App() {
   const [user, setUser] = useState();
@@ -18,24 +19,25 @@ function App() {
     <Router>
       <UserContext.Provider value={{ user, setUser }}>
         <HubContext.Provider value={{ hub, setHub }}>
-        <div className="App">
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+          <div className="App">
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
 
-            {/* Protected Routes */}
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/lifestory" element={<LifeStory />} />
-            <Route
-              path="/lifestory/:category"
-              element={<LifeStoryCategory />}
-              />
-            <Route path="/familytree" element={<FamilyTree />} />
-          </Routes>
-        </div>
-              </HubContext.Provider>
+              {/* Protected Routes */}
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/lifestory" element={<LifeStory />} />
+              <Route
+                path="/lifestory/:category"
+                element={<LifeStoryCategory />}
+                />
+              <Route path="/familytree" element={<FamilyTree />} />
+              <Route path="/hub" element={<HubAdmin/>} />
+            </Routes>
+          </div>
+        </HubContext.Provider>
       </UserContext.Provider>
     </Router>
   );
