1bf06293a94bb54f76e0faea2a45047da7518c0c
Alex Davis <alexdevdavis@gmail.com>
2022-07-06 14:55:16 +0100

    create basic hub creation component

--- client/all-for-one-fe/src/components/AssignHub.jsx
+++ client/all-for-one-fe/src/components/AssignHub.jsx
@@ -1,47 +1,50 @@
-import {React, useEffect, useContext, useState} from 'react';
+import {React, useContext, useState} from 'react';
 import { UserContext } from '../contexts/AuthProvider';
 import { HubContext } from '../contexts/HubProvider';
+import { checkValidHubName, createHub } from '../utils/api';
 
 export default function AssignHub() {
+    const {user} = useContext(UserContext);
+    const {hub, setHub} = useContext(HubContext);
+    const [isValidHub,setIsValidHub] = useState(false);
+    const [hubName, setHubName] = useState("");
+    const [hubNameCheck, setHubNameCheck] = useState("")
+
+     
     
-
-    const [formData, setFormData] = useState();
-
-    const handleSubmit = () => {
-
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      createHub({hubName, adminUser: user.user.email})
+      .then((hubData) => {
+      setHub(hubData)
+      });
     }
 
-    const updateFormData = (event) => {
-        const { id, value } = event.target;
-        setFormData((prevData) => {return {...prevData, [id]: value }});
+    const updateHubName = (event) => {
+        setHubNameCheck("")
+        setHubName(event.target.value);
+        checkValidHubName(hubName).then((isHubNameValid) => {
+          setIsValidHub(isHubNameValid);
+          if(!isValidHub){
+            setHubNameCheck("Sorry, that name is in use")
+          }
+        })
       };
 
   return (
     <>
       <form className="log-reg-form" onSubmit={handleSubmit}>
-          <label aria-label="hub-code">
+          <label aria-label="hub-name"> What do you want to call your hub?
           <input
             className="form-control"
-            id="hub-code"
-            value={formData.email}
+            id="hub-name"
+            value={hubName}
             placeholder="Enter hubcode"
-            onChange={updateFormData}
+            onChange={updateHubName}
           />
+          <p>{hubNameCheck}</p>
         </label>
-        <label aria-label="password">
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={formData.password}
-            placeholder="Enter your password"
-            onChange={updateFormData}
-          />
-        </label>
-        <button className="log-reg-btn">Sign In</button>
-        <Link className="log-reg-link-home" to="/register">
-          Not a member? <span>Register</span>
-        </Link>
+        <button className="log-reg-btn">Register Hub</button>
       </form>
     </>
   )
